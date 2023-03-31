8d51b20924b8ce841294e5ef4c08b5641b515e1f
YA-HSI Huang <ec211008@qmul.ac.uk>
2023-03-31 21:40:20 +0100

    Create register page and empty Login page Add some code in App.js to make register page work by YA-HSI

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,39 @@
 import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
+import {Register} from './Register'
+import {Login} from './Login'
 
 export default function App() {
-  const [backendData, setBackendData] = useState([]);
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-
-  const submit = () => {
-    const username = usernameRef.current.value;
-    const email = usernameRef.current.value;
-    const password = passwordRef.current.value;
-    axios.post('http://localhost:3333/login', {
-      username: username,
-      email: email,
-      password: password
-    })
-    .then(
-      response => setBackendData(response.data.message)
-    )
-    .catch( (err) => {
-      setBackendData(err.response.data.message);
+  // Register page
+    const [currentForm, setCurrentFrom] = useState('Register');
+  
+    const toggleForm = (formName) => {
+      setCurrentFrom(formName);
     }
-//testing
-    )
-  };
+  // Register page
+    const [backendData, setBackendData] = useState([]);
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    
+    const submit = () => {
+      const username = usernameRef.current.value;
+      const email = usernameRef.current.value;
+      const password = passwordRef.current.value;
+      axios.post('http://localhost:3333/login', {
+        username: username,
+        email: email,
+        password: password
+      })
+      .then(
+        response => setBackendData(response.data.message)
+      )
+      .catch( (err) => {
+        setBackendData(err.response.data.message);
+      }
+      )
+    }
   return (
-    <div>
+    <div classname="App">
       <input ref={usernameRef} placeholder="username/email" type="text" />
       <input ref={passwordRef} placeholder="password" type="text" />
       
@@ -33,8 +41,12 @@ export default function App() {
       {(() => {if (backendData.length > 0) {
         return (<p>{backendData}</p>)
       }
-    })()}
+      })()}
+      {
+        currentForm === "Register" ? <Register onFormSwitch={toggleForm}/> : <Login onFormSwitch={toggleForm}/>
+        // This is for Register page
+      }
     </div>
-  )
+  );
 }
 
