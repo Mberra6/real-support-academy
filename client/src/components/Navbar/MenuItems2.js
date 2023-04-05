6fb111a6ab14c472e8bb5b78f68de6d8ed8caf9f
vivian230 <efiannayiv@gmail.com>
2023-04-05 12:52:47 +0100

    Added a second MenuItems2 file(for only logged-in) user. And changed the vh of hero in home

---
+++ client/src/components/Navbar/MenuItems2.js
@@ -0,0 +1,24 @@
+export const MenuItems2 = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-links"
+    },
+    {
+        title : "About",
+        url : "/about",
+        cName : "nav-links"
+    },    
+    {
+        title : "Courses",
+        url : "/courses",
+        cName : "nav-links"
+    },
+    {
+        title : "Contact Us",
+        url : "/contact",
+        cName : "nav-links"
+    }, 
+
+]
+
