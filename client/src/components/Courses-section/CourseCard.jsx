ec58321e4b1191622b1237d5a814eadd3cc92636
Zahrashareef <91188380+Zahrashareef@users.noreply.github.com>
2023-04-08 01:18:06 +0100

    coursecards layout

--- client/src/components/Courses-section/CourseCard.jsx
+++ client/src/components/Courses-section/CourseCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 // implemented constructor with parameters intialised in Courses.jsx
+
+
 const CourseCard = (props) => {
   const { imgUrl, title, lesson, difficulty, rating } = props.item;
 
@@ -8,29 +10,25 @@ const CourseCard = (props) => {
     <div className="single__course__item">
       <div className="course__img">
         <img src={imgUrl} alt="" className="w-100" />
-
       </div>
 
       <div className="course__details">
         <h6 className="course__title mb-4">{title}</h6>
 
-{/* https://blade-ui-kit.com/ -> link for the componenets 'ri'*/}
-      <p className="lesson">
-        <span className="material-symbols-outlined book">menu_book
-           </span>
+        <div className="info__container">
+          <div className="info__item lesson">
+            <span className="material-symbols-outlined book">menu_book</span>
             {lesson} Lessons
-      </p>
-      <span className="material-symbols-outlined Difficulty">monitoring
-         </span> <p className="difficulty">
-             <i></i> {difficulty}
-           </p>
+          </div>
+          <div className="info__item difficulty">
+            <span className="material-symbols-outlined Difficulty">monitoring</span> {difficulty}
+          </div>
+        </div>
 
-
-        <div className="space">
+        <div className="enroll__container">
           <p className="enroll">
-             <a href="#"> Enroll Now</a>
+            <a href="#"> Enroll Now</a>
           </p>
- 
         </div>
       </div>
     </div>
